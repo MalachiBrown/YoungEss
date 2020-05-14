b5577ab2066afce047bab00b684c1f73f3bf9fa4
MalachiBrown <malachibiz@hotmail.com>
2020-05-14 18:42:36 +0100

    mobile query added for window resize

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ function displayMenu() {
 let vh = window.innerHeight * 0.01;
 
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
